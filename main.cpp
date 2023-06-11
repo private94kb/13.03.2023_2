e8d988526cfad23dd1c184cd8b745ef4f540989f
private94kb <91588734+private94kb@users.noreply.github.com>
2023-06-12 00:45:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
+
+bool isLeap(int year);
+int monthLength(int year, int month);
+
+int main(void) {
+for(int yr = 1995; yr < 2023; yr++)
+cout << yr << " -> " << isLeap(yr) << endl;
+  
+for(int yr = 1999; yr < 2004; yr++) {
+  for(int mo = 1; mo <= 12; mo++)
+    cout << monthLength(yr,mo) << " ";
+    cout << endl;
+}
+return 0;
+}
+
+bool isLeap(int year) {
+ if(year % 4 == 0){
+   return true;
+ }
+  else return false;
+}
+
+int monthLength(int year, int month) {
+ int days;
+  switch(month){
+    case 1: days = 31;break;
+    case 2: if(isLeap(year)) days = 29;
+      else days = 31; break;
+    case 3: days = 28;break;
+    case 4: days = 31;break;
+    case 5: days = 30;break;
+    case 6: days = 31;break;
+    case 7: days = 30;break;
+    case 8: days = 31;break;
+    case 9: days = 31;break;
+    case 10: days = 30;break;
+    case 11: days = 31;break;
+    case 12: days = 30;break;
+  }
+  return days;
+}
 
